05522c4750b68b8dd0a3dcaed8ed4e5e3ee6cb72
Meng Xu <meng.xu.cs@uwaterloo.ca>
2022-08-15 23:05:33 -0400

    x

--- tests/saw/config.py
+++ tests/saw/config.py
@@ -5,7 +5,7 @@ import os
 PATH_BASE = os.path.abspath(os.path.join(__file__, ".."))
 PATH_DEPS = os.path.join(PATH_BASE, "deps")
 PATH_DEPS_LLVM = os.path.join(PATH_DEPS, "target", "llvm-3.9.1")
-PATH_DEPS_SAW = os.path.join(PATH_DEPS, "target", "saw-0.9")
+PATH_DEPS_SAW = os.path.join(PATH_DEPS, "target", "saw-nightly")
 
 # misc
 NUM_CORES = multiprocessing.cpu_count()
