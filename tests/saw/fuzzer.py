0bcddac9e12b9bfdbcdc0c67f883c7be6d37eb82
Ji Ru <rubujubi@gmail.com>
2023-04-07 16:51:48 -0400

    Update fuzzer.py

--- tests/saw/fuzzer.py
+++ tests/saw/fuzzer.py
@@ -390,11 +390,7 @@ def _fuzzing_thread(tid: int) -> None:
         # new_trace[0].second_mutation is monitored here 
         # in order to prevent the seeds which are unable to do a second mutation become seed
 
-        # Also filter out the const flip type
-        if new_trace[0].rule == "const-replace" and new_trace[0].package["action"] == "flip":
-            continue
-        if not new_trace[0].second_mutation :
-            continue
+
         # create a new seed and register it to the seed queue
         new_seed = Seed.new_seed(
             new_trace, new_cov, novelty_marks - (len(new_cov) * 5) - len(new_trace)
